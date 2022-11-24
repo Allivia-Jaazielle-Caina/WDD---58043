5f38fcdab0e3b769e9a3f723cf23d7e9c637797f
Allivia Jaazielle P. Caiña <83685625+Allivia-Jaazielle-Caina@users.noreply.github.com>
2022-11-24 17:07:18 +0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,24 @@
+<html>
+	<head>
+		<title>My Login Page</title>
+	</head>
+	<body bgcolor = "aqua blue">
+	<form action = "connection.php" method = "post">
+		<p>
+			<font color = "blue" face = "Helvetica">
+			<label>Username:</label>
+			<input type = "text" id = "text" name = "user"/>
+			</font>
+		</p>
+		<p>
+			<label>Password:</label>
+			<input type = "password" id = "text" name = "pass"/>
+		</p>
+		<p>
+			<input type = "submit" id = "btn" name = "Login"/>
+		</p>
+	</form>
+	</body>
+	<?php
+	?>
+</html>
