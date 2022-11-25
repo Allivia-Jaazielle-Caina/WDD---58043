aae9989261778eebfd7d0235eafc0eca5fbcfe02
Allivia Jaazielle P. Caiña <83685625+Allivia-Jaazielle-Caina@users.noreply.github.com>
2022-11-25 09:26:13 +0800

    Add files via upload

---
+++ login_acoes_page.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+<title>My login form</title>
+</head>
+<body>
+	<body bgcolor = "#0D5B70">
+	<form action = "connection.php" method = "post">
+	<center><img src="acoes_logoooo.png" style="width: 1.2in; height: 1.67in;"></center>
+
+<center><h1 style="color:white">Adamson Computer Engineering Society</h1></center>
+
+		<p>
+		<center>
+			<label>Username:</label>
+			<input type = "text" id = "text" name = “user” placeholder="Username"> 
+		</center>
+		</p>
+		
+		<p>
+		<center>
+			<label>Password:</label>
+			<input type = "password" id = "text" name = “pass” placeholder="Password">
+		</center>
+		</p>
+		
+		<p>
+		<center>
+			<input type = "submit" id = "btn" name = "Login"/ >
+			</center>
+		</p>
+	</form>
+</body>
+</html>
